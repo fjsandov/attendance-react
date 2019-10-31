8503a845843daddc1a5b2c7469d07cd6b11cd489
Francisco Sandoval <francisco@mtblc.co>
2019-10-31 12:04:09 -0300

    feat: add bootstrap

---
+++ src/components/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavBar from './NavBar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export default ({ children }) => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <NavBar />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
